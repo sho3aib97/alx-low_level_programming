c8bfcc643838b8a613607dbce29266f36f9dc674
shoaib <mohamedshoaib401@gmail.com>
2023-08-14 14:59:55 -0700

    df

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main : entry point
+ * description : a c program that prints using puts function
+ * return : always 0 (succes)
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
