d6484ddc78ee98c6997083f2cc02f79566ea240b
shoaib <mohamedshoaib401@gmail.com>
2023-08-15 12:54:55 -0700

    task1

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry poinit
+ *
+ * Description: A C program that prints "_putchar"
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	puts("_putchar");
+	return (0);
+}
