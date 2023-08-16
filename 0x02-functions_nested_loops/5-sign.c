8bffa90900d07e4650d90ffc7aea643465ba9adf
shoaib <mohamedshoaib401@gmail.com>
2023-08-15 17:03:19 -0700

    dsw

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_sign - A function that prints the sin of n
+ *
+ * @n: input
+ *
+ * Return: 1 if +, 0 if 0, -1 if -
+*/
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+	else
+	{
+		_putchar(48);
+		return (0);
+	}
+}
