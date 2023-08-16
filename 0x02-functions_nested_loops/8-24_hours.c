4002cf5912f1daf2cb923a07043366f970a9ca10
shoaib <mohamedshoaib401@gmail.com>
2023-08-16 04:10:11 -0700

    dsfef

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * jack_bauer - A function that print every minutes of his day
+*/
+
+void jack_bauer(void)
+{
+	int m, h;
+
+	for (h = 0; h <= 23; h++)
+	{
+		for (m = 0; m <= 59; m++)
+		{
+			_putchar((h / 10) + 48);
+			_putchar((h % 10) + 48);
+			_putchar(':');
+			_putchar((m / 10) + 48);
+			_putchar((m % 10) + 48);
+			_putchar('\n');
+		}
+	}
+}
