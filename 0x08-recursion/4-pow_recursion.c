b02ffd0389f9f8547d8499f755e10bf2da7aed65
shoaib <mohamedshoaib401@gmail.com>
2023-08-30 17:34:42 -0700

    power

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -17,6 +17,7 @@ int _pow_recursion(int x, int y)
 		return (-1);
 	else if (y == 0)
 		return (1);
-	result = x * _pow_recursion(x, (y - 1))
+
+	result = x * _pow_recursion(x, (y - 1));
 	return (result);
 }
