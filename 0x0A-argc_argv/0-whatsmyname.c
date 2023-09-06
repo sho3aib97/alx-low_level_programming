5cd38d524b24ce8858d459be8215d8a987ed5615
shoaib <mohamedshoaib401@gmail.com>
2023-09-06 12:48:07 -0700

    program name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -11,11 +11,7 @@
 
 int main(int argc, char *argv[])
 {
-	int i;
 
-	for (i = 0; argv[0][i] != '\0'; i++)
-		printf("%s", argv[0][i]);
-	_putchar('\n');
-
+	printf("%s\n", argv[0]);
 	return (0);
 }
