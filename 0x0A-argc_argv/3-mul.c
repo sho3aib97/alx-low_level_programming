e6f6fb33a36fd19ffa2672294cd839f9fab1353b
shoaib <mohamedshoaib401@gmail.com>
2023-09-06 13:27:24 -0700

    multiplication

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * main - return the muliplication of two numbers
+ *
+ * @argc: the number of arguments
+ * @argv: arraay of strings to arugments
+ *
+ * Return: the result of the multiplication
+*/
+
+int main(int argc; char *argv[])
+{
+	int x,y,result;
+	
+	x = (int)argv[1];
+	y = (int)argv[2];
+
+	if (x == NULL || y == NULL || argc > 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	result = x * y;
+
+	return (result);
+}
