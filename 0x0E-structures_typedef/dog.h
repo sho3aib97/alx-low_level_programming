a2126434d8dbaaec62eb36788c9421db5f43e5f7
shoaib <mohamedshoaib401@gmail.com>
2023-09-11 08:35:18 -0700

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structure of data type dog
+ *
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ * Description: a collection of dogs name, age and owner
+*/
+
+struct dog
+{
+	char *name;
+	float *age;
+	char *owner;
+};
+#endif
