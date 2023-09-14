a2d14dd09bdf13b0ff64cc212b7949b05f1bfa7a
shoaib <mohamedshoaib401@gmail.com>
2023-09-14 12:30:24 -0700

    sum

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -4,6 +4,7 @@
  * sum_them_all - return the value of sum of all arguments
  *
  * @n: required argument
+ * @...: variable arguments
  *
  * Return: value of sum
 */
@@ -12,10 +13,10 @@ int sum_them_all(const unsigned int n, ...)
 {
 	unsigned int i;
 	int sum = 0;
-
-	if (!n || n == 0)
-		return (0);
 	va_list arg;
+
+	if (!n)
+		return (0);
 
 	va_start(arg, n);
 	for (i = 0; i < n; i++)
