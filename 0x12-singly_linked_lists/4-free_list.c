e7a157a137aac0631a02d72cdd158db50b245441
shoaib <mohamedshoaib401@gmail.com>
2023-09-21 13:34:40 -0700

    free

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -10,11 +10,12 @@ void free_list(list_t *head)
 {
 	if (head)
 	{
-		if (head->next)
-			free_list(head->next)
-		if (head->str)
-			free(head->str);
 		if (head->next != NULL)
+			free_list(head->next)
+		free(head->str);
+		if (head->next)
 			free(head->next);
 	}
+	else
+		return;
 }
