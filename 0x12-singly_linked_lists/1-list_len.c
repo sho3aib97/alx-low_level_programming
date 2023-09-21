f05bde795e79fb5862a9891252c61bb327f5a072
shoaib <mohamedshoaib401@gmail.com>
2023-09-21 12:54:54 -0700

    add node

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,6 +1,21 @@
 #include "lists.h"
 
 /**
+* _strlen - return length of string
+* @str: string to be calculated
+* Return: length
+*/
+
+unsigned int _strlen(const char *str)
+{
+	unsigned int i = 0;
+
+	while (str[i])
+		i++;
+	return (i);
+}
+
+/**
  * list_len - return list's length
  * @h: pointer to first element
  * Return: length of list
