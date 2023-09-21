320f226a6eb8ee6d62aea7186ae0b659e78d5f3d
shoaib <mohamedshoaib401@gmail.com>
2023-09-21 10:16:49 -0700

    list length

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+
+/**
+ * list_len - return list's length
+ * @h: pointer to first element
+ * Return: length of list
+*/
+
+size_t list_len(const list_t *h)
+{
+	size_t n = 1;
+
+	if (h->next)
+		n += list_len(h->next);
+	return (n);
+}
