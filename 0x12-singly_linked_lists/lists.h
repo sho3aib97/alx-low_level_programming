40aa249601de73f23c66be011b57b584f2870406
shoaib <mohamedshoaib401@gmail.com>
2023-09-21 09:45:45 -0700

    print list

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -3,6 +3,20 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+/**
+ * struct list_t - (singly) linked list
+ * @str: a string
+ * @len: the length of string
+ * @next: pointer to next node
+ * Description: (singly linked list)
+*/
+
+typedef struct list_t
+{
+	char *str;
+	unsigned int len;
+	struct list_t *next;
+} list_t;
 
 int _putchar(char c);
 size_t print_list(const list_t *h);
