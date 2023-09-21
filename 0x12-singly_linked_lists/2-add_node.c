3bca988455a081af8fba7469874b43e087516962
shoaib <mohamedshoaib401@gmail.com>
2023-09-21 12:31:45 -0700

    add node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,21 +1,6 @@
 #include "lists.h"
 
 /**
- * _strlen - return length of string
- * @str: string to be calculated
- * Return: length
-*/
-
-unsigned int _strlen(const char *str)
-{
-	unsigned int i = 0;
-
-	while (str[i])
-		i++;
-	return (i);
-}
-
-/**
  * add_node - adds a node to the first of the list
  * @head: pointer to a pointer of first element of list
  * @str: new string to be added
---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+
+/**
+ * add_node_end - add node to the end of the list
+ * @head: pointer to list
+ * @str: new string
+ * Return: address of new item
+*/
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *temp = malloc(sizeof(list_t));
+	list_t *end = *head;
+
+	if (!temp || !head)
+		return (NULL);
+	if (str)
+	{
+		temp->str = strdup(str);
+		if (!temp->str)
+		{
+			free(temp->str);
+			return (NULL);
+		}
+		temp->len = _strlen(temp->str);
+	}
+	if (end)
+	{
+		while (end->next)
+			end = end->next;
+			end->next = temp;
+	}
+	else
+		*head = temp;
+	return (temp);
+}
