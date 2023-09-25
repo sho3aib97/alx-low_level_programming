60368f3ba8446a6527ab41d3b48f21e59e294702
shoaib <mohamedshoaib401@gmail.com>
2023-09-25 07:44:32 -0700

    old task

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - print all possible single digits
+ * Return: zero always success
+*/
+
+int main(void)
+{
+	int n = 0;
+
+	while (n <= 9)
+	{
+
+		putchar('0' + n);
+		if (n != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		else
+			putchar('$');
+		n++;
+	}
+	return (0);
+}
