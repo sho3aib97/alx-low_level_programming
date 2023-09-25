a0283bc1d58509a047a35559f114b6b4dcfe2efe
shoaib <mohamedshoaib401@gmail.com>
2023-09-25 11:53:04 -0700

    old task

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,6 +1,24 @@
 #include "main.h"
 
 /**
+ * _memset - assign memory to zero bytes
+ * @str: pointer to memory
+ * @zero: zero bytes
+ * @i: size of memory
+ * Return: pointer to first element
+*/
+
+char *_memset(char *str, char zero, unsigned int i)
+{
+	char *pointer = str;
+
+	while (i--)
+		*str++ = zero;
+
+	return (pointer);
+}
+
+/**
  * _calloc - allocate memory to an array and set it to 0
  *
  * @nmemb: number of the elements within the array
@@ -16,10 +34,10 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	array = malloc(size * nmemb);
+	array = malloc(sizeof(int) * nmemb);
 
 	if (array == NULL)
 		return (NULL);
-
+	_memset(array, 0, sizeof(int) * nmemb);
 	return (array);
 }
