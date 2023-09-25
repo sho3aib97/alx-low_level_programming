2ceca4a0149f8bbe6f6d5a2fd1a912730835924e
shoaib <mohamedshoaib401@gmail.com>
2023-09-25 11:44:37 -0700

    old task

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,24 +11,15 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	int i;
-	int *arr;
+	void *array;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	arr = malloc(nmemb * size);
+	array = malloc(size * nmemb);
 
-	if (arr == NULL)
+	if (array == NULL)
 		return (NULL);
 
-	for (i = 0; i < size; i++)
-		arr[i] = 0;
-
-	while (i--)
-	{
-		if (arr[i] != 0)
-		free(arr);
-	}
-	return ((void *)arr);
+	return (array);
 }
