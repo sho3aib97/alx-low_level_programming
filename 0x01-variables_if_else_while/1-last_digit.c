538b5401106fba39b462233da5922a4e31d1641d
shoaib <mohamedshoaib401@gmail.com>
2023-09-25 06:43:53 -0700

    old task

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -20,6 +20,6 @@ int main(void)
 	else if (n % 10 == 0)
 		printf("%d and is 0\n", n % 10);
 	else if (n % 10 < 6 && n % 10 != 0)
-		printf("%d and is less than 6 and not 0\n");
+		printf("%d and is less than 6 and not 0\n", n % 10);
 	return (0);
 }
