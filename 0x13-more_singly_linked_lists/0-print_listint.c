d0a1945be5422cd5b6546f4f9f4b1846f374fd4d
shoaib <mohamedshoaib401@gmail.com>
2023-09-26 06:10:20 -0700

    insert node

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -14,8 +14,7 @@ size_t print_listint(const listint_t *h)
 
 	if (!h)
 		return (0);
-	if (h->n)
-		printf("%d\n", h->n);
+	printf("%d\n", h->n);
 
 	if (h->next != NULL)
 		i += print_listint(h->next);
--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,6 +1,23 @@
 #include "lists.h"
 
 /**
+ * listint_len - return length of single linked list
+ * @h: head pointer to the list
+ * Return: size_t value of length
+*/
+
+size_t listint_len(const listint_t *h)
+{
+	size_t i = 1;
+
+	if (!h)
+		return (0);
+	if (h->next)
+		i += listint_len(h->next);
+	return (i);
+}
+
+/**
  * insert_nodeint_at_index - insert a node in a specific index
  * @head: pointer to first node
  * @idx: specific index to insert into
