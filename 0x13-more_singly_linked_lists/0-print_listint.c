af892603f27c53264082cab36935ae31a34981a3
shoaib <mohamedshoaib401@gmail.com>
2023-09-25 23:35:05 -0700

    print list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * print_listint - print linked list and return num of nodes
+ *
+ * @h: head pointer to linked list
+ *
+ * Return: number of nodes
+*/
+
+size_t print_listint(const listint_t *h)
+{
+	size_t i = 1;
+
+	if (!h)
+		return (0);
+	if (h->n)
+		printf("%d\n", h->n);
+
+	if (h->next != NULL)
+		i += print_listint(h->next);
+
+	return (i);
+}
---
+++ 0x13-more_singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+read me file is ready in second day
