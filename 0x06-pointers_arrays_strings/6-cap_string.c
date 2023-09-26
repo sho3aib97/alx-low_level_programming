cb8c5f44d8e465cb137c58d461b92c8787bac7bf
shoaib <mohamedshoaib401@gmail.com>
2023-09-26 10:13:22 -0700

    string cap

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,56 @@
+#include "main.h"
+
+/**
+ * _islower - determine lowercase or not
+ * @c: character
+ * Return: 1 if true
+*/
+
+int _islower(char c)
+{
+	return (c >= 97 && c <= 122);
+}
+
+/**
+ * _isdelimiter - determine is it delimiter or not
+ * @c: character
+ * Return: 1 if true
+*/
+
+int _isdelimiter(char c)
+{
+	int n;
+	char _delimiter[] = "\t\n,.!?\"(){}";
+
+	for (n = 0; n < 12; n++)
+		if (c == _delimiter[n])
+			return (1);
+	return (0);
+}
+
+/**
+ * cap_string - turn all words into capitals
+ * @s: string
+ * Return: pointer to new string
+*/
+
+char *cap_string(char *s)
+{
+	char *pointer = s;
+	int fDelimiter = 1;
+
+	while (*s)
+	{
+		if (_isdelimiter(*s))
+			fDelimiter = 1;
+		else if (_islower(*s) && fDelimiterer)
+		{
+			*s -= 32;
+			fDelimiter = 0;
+		}
+		else
+			fDelimiter = 0;
+		s++;
+	}
+	return (pointer);
+}
