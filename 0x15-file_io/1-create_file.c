578efd193f0cc1d1739a6f2a1fe97c166b6fcb96
shoaib <mohamedshoaib401@gmail.com>
2023-10-06 00:46:55 -0700

    ok

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * _strlen - get length of string
+ * @str: string to be calculated
+ * Return: integer value
+*/
+
+int _strlen(char *str)
+{
+	int n =  0;
+
+	if (!str)
+		return (0);
+	while (*str++)
+		n++;
+	return (i);
+}
+
+/**
+ * create_file - create a new file
+ * @filename: name of new file
+ * @text_content: content of file
+ * Return: integer value
+*/
+
+int create_file(const char *filename, char *text_content)
+{
+	int fil;
+	ssize_t size = 0;
+
+	length = _strlen(text_content);
+	if (!filename)
+		return (-1);
+	fil = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fil == -1)
+		return (-1);
+	if (length)
+		size = write(fil, text_content, length);
+	close(fil);
+	return (size == length ? 1 : -1);
+}
